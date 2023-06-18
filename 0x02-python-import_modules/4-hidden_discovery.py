f94c90c522a675ea93b8314f79e9c9c25286c624
Loay-Ahmed <loayabelgendy@gmail.com>
2023-06-18 10:38:21 -0400

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    with open("hidden_4.pyc", "r") as f:
-        for line in f:
-            if line[0] != "__":
-                print(line)
+    import hidden_4
+    f = dir(hidden_4)
+    for line in f:
+        if line[:2] != "__":
+            print(line)
