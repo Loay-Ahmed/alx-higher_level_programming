682dcc5c05fa78bcbdd3d9f22714c96a320e03ec
Loay-Ahmed <loayabelgendy@gmail.com>
2023-06-26 06:41:37 -0400

    Task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,5 +7,6 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             ele += 1
         except IndexError:
-            print("\n")
-            return ele
+            break
+    print()
+    return ele
