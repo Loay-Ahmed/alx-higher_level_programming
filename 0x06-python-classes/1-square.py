d3c70713aee12ed909a8d08018d6f48d97e61d67
Loay-Ahmed <loayabelgendy@gmail.com>
2023-06-27 14:56:43 -0400

    Task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+"""
+This module is for Task 1
+"""
+
+
+class Square:
+    """Square class with __init__ function"""
+
+    def __init__(self, size):
+        """
+        Class initialization
+
+        Args:
+            size: square size
+        """
+        self.__size = size
